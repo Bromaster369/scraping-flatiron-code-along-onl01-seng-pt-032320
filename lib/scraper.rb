485d25a8416e7ccb46e49061c01243564955f4c4
Christian Pardo <chrispardopersonal@gmail.com>
2020-05-04 01:40:58 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,12 +28,13 @@ class Scraper
     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
-        puts ""
-    
-    
-    
+        puts "Title: #{course.title}"
+        puts "Schedule: #{course.schedule}"
+        puts "Title: #{course.description}"
+      end
+    end
   end 
-  
+    
 end
 
 
